2b7a6dbd8513f6d16641389f69d421ca9e37a87a
Tyler Burns <Tburnsie10@gmail.com>
2023-04-29 14:37:22 -0400

    fine details

---
+++ script.js
@@ -0,0 +1,60 @@
+console.log(document.querySelector('.container'))
+
+function submitForm(event) {
+
+    event.preventDefault();
+    // event.target is the form, with querySelector('input') we are getting all inputs inside the form.
+    let inputs = event.target.querySelectorAll('input');
+    
+    let first = inputs[0].value
+    let last = inputs[1].value
+    let idNum = inputs[2].value
+    let title = inputs[3].value
+    let annualSal = inputs[4].value
+    
+
+    let tBody = document.querySelector('#tbody')
+
+    tBody.innerHTML +=  `
+    <tr>
+        <td>${first}</td>
+        <td>${last}</td>
+        <td>${idNum}</td>
+        <td>${title}</td>
+        <td id="salary">${annualSal}</td>
+        <td><button onclick="deleteRow(event)" >delete</button></td>
+    </tr> 
+`
+    
+
+    updateCost();
+}  
+
+function deleteRow(event) {
+    
+    event.target.parentElement.parentElement.remove()
+
+    updateCost();
+}
+
+
+function updateCost() {
+    let sal = document.querySelectorAll('#salary')
+    let total = 0;
+    for(let item of sal) {
+        total += Number(item.innerHTML) 
+    }
+
+    document.querySelector('#cost').innerHTML = '$' + (total / 12).toFixed(2);
+
+    if(total / 12 > 20000) {
+        document.querySelector('#cost').style = 'background-color: red;'
+    } else {
+        document.querySelector('#cost').style = 'background-color: lightgreen;'
+    }
+    
+}
+
+
+
+
